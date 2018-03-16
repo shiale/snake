12fe8cf9288e452c4e08acd1d7c0fef2e04400c5
DESKTOP-DOG1C89\shiale <primer@primer.com>
2018-03-17 02:51:46 +0600

    Процедурный подход

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,8 +10,23 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            int x1 = 1; //int - 32 разрядное (4 байта) целое число со знаком от -2 147 483 648 до 2 147 483 647
+            int y1 = 3;
+            char sym1 = '*'; //char - 16 разрядное числовое значение (символ Юникод-16)
+            Draw(x1, y1, sym1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+            Draw(x2, y2, sym2);
+
             Console.ReadKey();
+        }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
