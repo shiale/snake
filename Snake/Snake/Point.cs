d35b24efde84ad15d06fa784c27ea506f4cdf7df
DESKTOP-DOG1C89\shiale <primer@primer.com>
2018-03-17 03:46:38 +0600

    Classes

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point
+    {
+        //int - 32 разрядное (4 байта) целое число со знаком от -2 147 483 648 до 2 147 483 647
+        public int x; 
+        public int y;
+        //char - 16 разрядное числовое значение (символ Юникод-16)
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
