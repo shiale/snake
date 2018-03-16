ebc56320ff8a7b89a4e84a52a86a82dcec6f5a42
DESKTOP-DOG1C89\shiale <primer@primer.com>
2018-03-17 04:34:20 +0600

    Добавил конструктор.

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -14,6 +14,16 @@ namespace Snake
         //char - 16 разрядное числовое значение (символ Юникод-16)
         public char sym;
 
+        //Конструктор имеет специальный синтаксис
+        //1. Он называется так же как класс (в моем случае Point)
+        //2. Опускается тип возвращаемого значения т.к. конструктор ничего не возвращает (или возвращает свой тип?)
+        public Point(int x=0, int y=0, char sym='0')
+        {
+            this.x = x;
+            this.y = y;
+            this.sym = sym;
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
